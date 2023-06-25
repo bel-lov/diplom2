c8c1435463dafb32907aa5322e73ba97d73297ab
bel-lov <belou.lyuba@yandex.ru>
2023-06-25 21:38:02 +0300

    commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,29 @@
-// const react = require('eslint-plugin-react');
-// const globals = require('globals');
+const react = require('eslint-plugin-react');
+const globals = require('globals');
 
-// module.exports = [
-//     {
-//         files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-//         plugins: {
-//             react,
-//         },
-//         languageOptions: {
-//             parserOptions: {
-//                 ecmaFeatures: {
-//                     jsx: true,
-//                 },
-//             },
-//             globals: {
-//                 ...globals.browser,
-//             },
-//         },
-//         rules: {
-//             // ... any rules you want
-//             'react/jsx-uses-react': 'error',
-//             'react/jsx-uses-vars': 'error',
-//             "react/prop-types": 0,
+module.exports = [
+    {
+        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+        plugins: {
+            react,
+        },
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+            },
+        },
+        rules: {
+            // ... any rules you want
+            'react/jsx-uses-react': 'error',
+            'react/jsx-uses-vars': 'error',
+            "react/prop-types": 0,
 
-//         },
-//         // ... others are omitted for brevity
-//     },
-// ];
+        },
+        // ... others are omitted for brevity
+    },
+];
